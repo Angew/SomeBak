4c6209034352202a7b1cdc2e3537ed4804537fb0
Petr Kmoch <petr.kmoch@gmail.com>
2016-09-01 22:31:07 +0200

    Add legal header
    Add copyright & license header to SomeBak.py

--- SomeBak.py
+++ SomeBak.py
@@ -1,3 +1,7 @@
+# Copyright Petr Kmoch 2016
+# Distributed under the Boost Software License, Version 1.0.
+# (See accompanying file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
+
 import os
 import os.path
 import shutil
