3975876d64698dc8967e38f98f55c8cd47b7f7f9
Petr Kmoch <petr.kmoch@gmail.com>
2016-09-04 21:16:54 +0200

    Config file processing
    Read file names from config file

--- code/SomeBak.py
+++ code/SomeBak.py
@@ -27,6 +27,13 @@ class Program:
 			dest = 'targetLocation'
 		)
 		parser.add_argument(
+			'--config-file-name',
+			help = 'Alternative name of config files',
+			metavar = 'NAME',
+			default = 'SomeBak.cfg',
+			dest = 'configFileName'
+		)
+		parser.add_argument(
 			'configDir',
 			help = 'Directory with initial configuration file',
 			metavar = 'config',
@@ -39,9 +46,50 @@ class Program:
 	def setOptions(self, options):
 		self.targetLocation = os.path.abspath(options.targetLocation)
 		self.configDir = os.path.abspath(options.configDir)
+		self.configFileName = options.configFileName
+
+
+	def processConfigDir(self, configDir):
+		try:
+			artefacts = []
+			with open(os.path.join(configDir, self.configFileName)) as configFile:
+				for line in configFile:
+					artefact = self.processConfigLine(line)
+					if artefact is not None:
+						artefacts.append(artefact)	# WIP: needs to be an object with data
+			return artefacts
+		except OSError as err:
+			# ToDo: log error
+			return []
+
+
+	def processConfigLine(self, line):
+		# ToDo: fullPath, targetPath
+		file = ''
+		escaped = False
+		for ch in line:
+			if escaped:
+				if ch in {'`', '|'}:
+					file += ch
+					escaped = False
+				else
+					# ToDo: log error (unknown escape)
+					return ''
+			else:
+				if ch == '`':
+					escaped = True
+				elif ch == '|':
+					return file
+				else:
+					file += ch
+		if escaped:
+			# ToDo: log error (loose escape)
+			return ''
+		return file
 
 
 	def run(self):
+		self.fileArtefacts = self.processConfigDir(self.configDir)
 		for sourceFile in self.fileArtefacts:
 			os.makedirs(sourceFile.targetPath, exist_ok = True)
 			shutil.copy2(sourceFile.fullPath, os.path.join(self.targetLocation, sourceFile.targetPath))
